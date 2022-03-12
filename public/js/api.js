80c112ea102eb1f545cb412c50a253facee70921
Brilian Natanael Zega <fttmbril22@gmail.com>
2022-03-13 02:56:33 +0700

    Add files via upload

---
+++ public/js/api.js
@@ -0,0 +1,9 @@
+// My api code
+let api_key = "02f35a3cc88f5fe5967d821301d8d6ad";
+
+// url from themoviedb.org
+let img_url = "https://image.tmdb.org/t/p/w500";
+let original_img_url = "https://image.tmdb.org/t/p/original";
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
+let movie_detail_http = "https://api.themoviedb.org/3/movie";
